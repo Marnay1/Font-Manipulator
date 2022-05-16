8379d27a15c46b67cabef452ee53aafa23cf3c4a
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-05-16 18:07:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftWristX= 0;
+rightWristX= 0;
+difference= 0;
 function preload(){
 
 }
@@ -15,5 +18,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length> 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        difference= floor(leftWristX-rightWristX);
+        
     }
+}
+function draw(){
+    background("brown");
+    textSize(difference);
+    fill("skyblue");
+    text("Marnay", 40, 200)
 }
